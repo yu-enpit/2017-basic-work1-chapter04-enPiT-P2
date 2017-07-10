42692ac5ccade615a8d17c3d75c3ca912256e2a1
enPiT-P2 <v002ff@yamaguchi-u.ac.jp>
2017-07-10 18:11:10 +0900

    Chapter4のプログラムの修正

--- Janken/app/src/main/java/com/example/u90684/janken/MainActivity.java
+++ Janken/app/src/main/java/com/example/u90684/janken/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.u90684.janken;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onJankenButtonTapped(View view) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("My_HAND", view.getId());
+        startActivity(intent);
     }
 }
